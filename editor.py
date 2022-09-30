be392bb703d28383d21ea184f8ea86a685afa2e9
RWXogenisis <65536776+RWXogenisis@users.noreply.github.com>
2022-10-01 02:06:58 +0530

    Update editor.py
    Fixed update function. Added update and read functions.

--- editor.py
+++ editor.py
@@ -16,7 +16,7 @@ def send():
     with open("chats.json", "r") as source:
         obj = json.load(source)
 
-    data = {"text_id": int(obj["chat_logs"][-1]["text_id"]) + 1,"sender_ID": sender_id , "receiver_ID":receiver_id ,"timestamp": str(datetime.datetime.now())[:-7], "message": message, "emoji": emoji, "reaction": ""}
+    data = {"text_id": str(int(obj["chat_logs"][-1]["text_id"]) + 1),"sender_ID": sender_id , "receiver_ID":receiver_id ,"timestamp": str(datetime.datetime.now())[:-7], "message": message, "emoji": emoji, "reaction": ""}
     obj["chat_logs"].append(data)
 
     with open("chats.json", "w") as dest:
@@ -25,14 +25,48 @@ def send():
 
 def update(text_id, sender_id, receiver_id):
     '''
-    Updating if the user gives any reactions
+    Updating if the user gives any reactions in chats.json
+    Parameters :(text_id, sender_id, receiver_id)
     '''
 
+    with open("chats.json", "r") as source:
+        obj = json.load(source)
+    for i in (obj["chat_logs"]):
+        if(i["text_id"] == text_id and i["sender_ID"] == sender_id and i["receiver_ID"] == receiver_id):
+            i["reaction"] = input()
+
+    with open("chats.json", "w") as dest:
+        json.dump(obj, dest, indent=4)
+
+
+def read():
+    '''
+    Display the contents of the json file
+    '''
     with open("chats.json", "r") as source:
         obj = json.load(source)
     for i in (obj["chat_logs"]):
         print(i)
 
 
+def delete(text_id, sender_id, receiver_id):
+    '''
+    To delete chats which were sent unintentionally
+    Parameters :(text_id, sender_id, receiver_id)
+    '''
+    with open("chats.json", "r") as source:
+        obj = json.load(source)
+    for i in obj["chat_logs"]:
+        if(i["text_id"] == text_id and i["sender_ID"] == sender_id and i["receiver_ID"] == receiver_id):
+            obj["chat_logs"].remove(i)
+
+    for i in obj["chat_logs"]:
+        print(i)
+    with open("chats.json", "w") as dest:
+        json.dump(obj, dest, indent=4)
+
+
 #send()
-update()
+#update(input(), input(), input())
+#read()
+#delete(input(), input(), input())
